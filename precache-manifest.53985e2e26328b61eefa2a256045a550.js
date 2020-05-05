8d8bfbc2b6f82152d60b0e4d30ad36281f57b9b9
Coupdag <sensineer@gmail.com>
2020-05-05 13:05:29 +0300

    Updates

---
+++ precache-manifest.53985e2e26328b61eefa2a256045a550.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87bb6cc01694a7b0723342fcfd490ee3",
+    "url": "/asd/index.html"
+  },
+  {
+    "revision": "0b46b57510f76e2d4efb",
+    "url": "/asd/static/css/main.8b31278c.chunk.css"
+  },
+  {
+    "revision": "97becdcaefe31bdfedb0",
+    "url": "/asd/static/js/2.795a6220.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/asd/static/js/2.795a6220.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b46b57510f76e2d4efb",
+    "url": "/asd/static/js/main.58ddaab8.chunk.js"
+  },
+  {
+    "revision": "cfcd47161b10722abdc3",
+    "url": "/asd/static/js/runtime-main.e801a2e2.js"
+  }
+]);
